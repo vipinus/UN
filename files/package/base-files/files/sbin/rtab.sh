0affbe951d7f09a1711ce23b9c8513959e57541b
vipinus <37501804+vipinus@users.noreply.github.com>
2025-04-29 21:26:35 -0400

    Update rtab.sh

--- files/package/base-files/files/sbin/rtab.sh
+++ files/package/base-files/files/sbin/rtab.sh
@@ -7,7 +7,7 @@ if [ ! -f "$file" ]; then
     iptables-nft -t nat -N REDSOCKS
     iptables-nft -t nat -A OUTPUT -p tcp --dport 22 -j RETURN
     iptables-nft -t nat -A OUTPUT -p tcp -j REDSOCKS
-    iptables-nft -t nat -A REDSOCKS -m set --match-set redsocks_bypass dst -j RETURN
+    iptables-nft -t nat -A REDSOCKS -m set ! --match-set redsocks_bypass dst -j RETURN
     iptables-nft -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 1081
     iptables-nft -t nat -A PREROUTING -p tcp -j REDSOCKS
     /etc/init.d/firewall restart
